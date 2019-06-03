ca37b8097b3d96f2959ea620a46dc8b32b890438
Koray Dundar <koraydundar_40@hotmail.com>
2019-06-03 10:34:38 +0300

    created dice, roll dice components and dice css

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React, { Component} from 'react';
 import './App.css';
+import RollDice from './components/RollDice'
+import Dice from './components/Dice'
 
 class App extends Component {
   render(){
 
     return (
-      <div>
+      <div className="App">
         <h1>App</h1>
+        <div className="dices">
+          <Dice face="five"/>
+          <Dice face="two"/>
+          <Dice face="four"/>
+          <Dice face="three"/>
+          <Dice face="one"/>
+          <Dice face="six"/>
+        </div>
       </div>       
     );
   }
---
+++ src/components/RollDice.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import Dice from './Dice'
+class RollDice extends Component {
+    render(){
+        return (
+            <div>
+                <h1>Roll Dice</h1>
+                
+            </div>
+        )
+    }
+}
+
+export default RollDice
